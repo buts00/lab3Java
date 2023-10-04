67f66ce3edfc03067ba0f0a9c7f175fc546c86f9
buts00 <butsprog@gmail.com>
2023-10-04 17:28:22 +0300

    first commit

---
+++ Practice.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.Comparator;
+import java.util.Collections;
+
+public class Practice {
+    public static Student findMostSuccessfulStudent(Vector<Student> students) {
+        Comparator<Student> scoreComparator = Comparator.comparingDouble(Student::getAverageScore);
+        Student mostSuccessfulStudent = Collections.max(students, scoreComparator);
+        return mostSuccessfulStudent;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Vector<Student> students = new Vector<>();
+
+        while (true) {
+            System.out.println("Enter one operations:");
+            System.out.println("1. Add new student");
+            System.out.println("2. Show information about student");
+            System.out.println("3. Find the most successful");
+            System.out.println("4. Clear data about students");
+            System.out.println("5. Exit");
+
+            int x = Integer.parseInt(scanner.nextLine());
+
+            if (x == 1) {
+                System.out.print("Name: ");
+                String name = scanner.nextLine();
+
+                System.out.print("University: ");
+                String university = scanner.nextLine();
+
+                System.out.print("Faculty: ");
+                String faculty = scanner.nextLine();
+
+                System.out.print("AverageScore: ");
+                double averageScore = Double.parseDouble(scanner.nextLine());
+                students.add(new Student(name, university, faculty, averageScore));
+            } else if (x == 2) {
+                if(students.isEmpty()){
+                    System.out.println("Empty data about students");
+                }else {
+                    int studentNumber = 1;
+                    for (Student student : students) {
+                        System.out.println("Details of Student " + studentNumber + ":");
+                        student.printInformation();
+                        System.out.println("----------------");
+                        studentNumber++;
+                    }
+                }
+            } else if(x==3){
+                if(students.isEmpty()){
+                    System.out.println("Empty data about students");
+                }
+                else {
+                    Student mostSuccessfulStudent = findMostSuccessfulStudent(students);
+                    System.out.print("The most successful student is: ");
+                    mostSuccessfulStudent.printInformation();
+                    System.out.println("----------------");
+                }
+            } else if(x==4){
+                System.out.println("All data was deleted");
+                students.clear();
+            } else {
+                System.out.println("The end of program");
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,51 @@
+public class Student {
+    private  String name;
+    private String university;
+    private String faculty;
+    private double  averageScore;
+    public Student(String name, String university, String faculty,  double averageScore) {
+        this.name = name;
+        this.university = university;
+        this.faculty = faculty;
+        this.averageScore  = averageScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(int averageScore) {
+        this.averageScore = averageScore;
+    }
+
+    public void printInformation() {
+        System.out.println("Student's name: " + getName());
+        System.out.println("University: " + getUniversity());
+        System.out.println("Faculty of: " + getFaculty());
+        System.out.println("Student's age: " + getAverageScore());
+    }
+}
